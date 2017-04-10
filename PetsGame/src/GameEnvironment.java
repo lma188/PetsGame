28cc17a81cf6421619291be1ec06d271da71b9bf
ltt19 <ltt19@uclive.ac.nz>
2017-04-10 16:37:50 +1200

    Add PetsGame super file
    Add PetsGame super file with all bin src, eclipse project stuff

---
+++ PetsGame/src/GameEnvironment.java
@@ -0,0 +1,26 @@
+/**
+ * The GameEnvironment class is the main class that runs the game.
+ * @author ltt19
+ * @version 1.0
+ *
+ */
+public class GameEnvironment {
+	
+	/**
+	 * @param playerList The list of player instances, the size of which is the number of players
+	 * @param speciesAvailable The list of species available for the player to choose from
+	 * @param numDays The number of "days" the game will run for, as specified by the player(s)
+	 * @param numPlayers The number of players in this instance of the game
+	 */
+
+	public Player[] playerList;
+	public Species[] speciesAvailable;
+	public int numDays;
+	public int numPlayers;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
