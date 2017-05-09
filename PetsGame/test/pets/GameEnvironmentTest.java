6686ae89ab75ec172769f1bc55e749425e46edc9
Lucy Turner <ltt19@uclive.ac.nz>
2017-05-09 13:09:32 +1200

    added game environment tester

---
+++ PetsGame/test/pets/GameEnvironmentTest.java
@@ -0,0 +1,270 @@
+/**
+ * 
+ */
+package pets;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author ltt19
+ *
+ */
+public class GameEnvironmentTest{
+	private GameEnvironment game;
+	private Pet pet;
+	private Player player;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		game = new GameEnvironment(1,1);
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		game = null;
+		pet = null;
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#confirmInput(java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testConfirmInput() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#askNumPlayers()}.
+	 */
+	@Test
+	public void testAskNumPlayers() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#askNumDays()}.
+	 */
+	@Test
+	public void testAskNumDays() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#chooseSpecies(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testChooseSpecies() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#printSpeciesListNum()}.
+	 */
+	@Test
+	public void testPrintSpeciesListNum() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#printSpeciesList()}.
+	 */
+	@Test
+	public void testPrintSpeciesList() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#chooseViewSpeciesStats()}.
+	 */
+	@Test
+	public void testChooseViewSpeciesStats() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#speciesStatsOrCon(java.lang.String, int)}.
+	 */
+	@Test
+	public void testSpeciesStatsOrCon() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#setUpPlayers()}.
+	 */
+	@Test
+	public void testSetUpPlayers() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#printStore()}.
+	 */
+	@Test
+	public void testPrintStore() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#askForPurchase(pets.Player)}.
+	 */
+	@Test
+	public void testAskForPurchase() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#goToStore(pets.Player)}.
+	 */
+	@Test
+	public void testGoToStore() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#goToInventory(pets.Player, pets.Pet)}.
+	 */
+	@Test
+	public void testGoToInventory() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#viewPetStats(pets.Pet)}.
+	 */
+	@Test
+	public void testViewPetStats() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#askNumPets(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testAskNumPets() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#askName(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testAskName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#payForTreatment(pets.Pet, pets.Player)}.
+	 */
+	@Test
+	public void testPayForTreatment() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#makePetSick(pets.Pet, pets.Player)}.
+	 */
+	@Test
+	public void testMakePetSick() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#punishPet(pets.Pet, pets.Player)}.
+	 */
+	@Test
+	public void testPunishPet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#makePetMisbehave(pets.Pet, pets.Player)}.
+	 */
+	@Test
+	public void testMakePetMisbehave() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#randomEventPicker(pets.Player, pets.Pet)}.
+	 */
+	@Test
+	public void testRandomEventPicker() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#printListPetChoices(java.util.ArrayList, pets.Player)}.
+	 */
+	@Test
+	public void testPrintListPetChoices() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#givePetOptions(pets.Player, pets.Pet)}.
+	 */
+	@Test
+	public void testGivePetOptions() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#willRevivePet(pets.Pet, pets.Player)}.
+	 */
+	@Test
+	public void testWillRevivePet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#updatePetAliveStats(pets.Player)}.
+	 */
+	@Test
+	public void testUpdatePetAliveStats() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#playGame()}.
+	 */
+	@Test
+	public void testPlayGame() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#finishGame()}.
+	 */
+	@Test
+	public void testFinishGame() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#askHelp()}.
+	 */
+	@Test
+	public void testAskHelp() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pets.GameEnvironment#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
